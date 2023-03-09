79daa9e8ab8c54399148ed519be96c74839c73e2
ultrakeypoint <73173310+ultrakeypoint@users.noreply.github.com>
2023-03-09 10:12:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-async-await-problem
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-zsaywv)
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  mounted() {
+    const asyncFunc = async () => {
+      let a = await this.resolve(2);
+      let b = await this.resolve(2);
+      let c = await this.resolve(2);
+
+      console.log('After all promise is executed');
+    };
+
+    asyncFunc();
+    console.log('Hello World');
+  },
+
+  methods: {
+    async resolve(time) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve('resolve');
+        }, time * 1000);
+      });
+    },
+  },
+};
+</script>
